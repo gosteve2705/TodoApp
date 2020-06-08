43427ec6f7021180d5ad57a264a8a8defa5a7ce2
gosteve <nomuojamaro@gmail.com>
2020-06-08 13:32:20 +0100

    sectioned the header and footer to be reused for layouts then created an about page

--- app.js
+++ app.js
@@ -2,34 +2,68 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+
+
 var tasks = ['buy food','cook food','eat food'];
+var worktasks = [];
+
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
 
+
 app.set('view engine', 'ejs');
 
+
+
 app.get("/", function(req, res){
-var today =  new Date();
-var options = {
+let today =  new Date();
+let options = {
   weekday: 'long',
    year: 'numeric',
    month: 'long',
     day: 'numeric'
    };
-var day =today.toLocaleDateString("en-US", options);
+let day =today.toLocaleDateString("en-US", options);
 
-res.render('list', {kindofday: day , newTask: tasks});
+res.render('list', {listTitle: day , newTask: tasks});
 
 });
+
+
 
 app.post('/',function(req,res){
-var   task = req.body.task ;
-console.log(task);
-tasks.push(task);
- res.redirect('/');
+
+let   task = req.body.task ;
+
+console.log(req.body.tasksList);
+
+if(req.body.tasksList === 'Work'){
+  worktasks.push(task);
+   res.redirect('/work');
+}else{
+  tasks.push(task);
+   res.redirect('/');
+
+}
+
+
 });
+
+
+
+app.get('/work',function (req,res){
+  res.render('list', {listTitle: 'Work Tasks' , newTask: worktasks});
+});
+
+
+app.get('/about',function (req,res){
+  res.render('about');
+});
+
+
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
